ffa3b32d126d23498bb90334076e28b29b5a0b1d
yzy98 <yz186337@163.com>
2026-05-30 21:20:07 +0800

    feat: add web package

---
+++ apps/web/src/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-amber-700 text-amber-100">
+      <p className="font-bold text-2xl">Hello Vite</p>
+    </div>
+  );
+}
+
+export default App;
