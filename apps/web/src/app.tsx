a2df533bc93b2f8122753383ade23dc9b1d5b620
yzy98 <yz186337@163.com>
2026-05-31 15:35:40 +0800

    feat: add shadcn ui monorepo setup

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@ext-stack/ui/components/button";
+import { ModeToggle } from "@/components/mode-toggle";
+
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-amber-700 text-amber-100">
-      <p className="font-bold text-2xl">Hello Vite</p>
+    <div>
+      <Button>test</Button>
+      <ModeToggle />
     </div>
   );
 }
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
 import App from "./app.tsx";
 
 const rootElement = document.getElementById("root");
@@ -11,6 +12,8 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
